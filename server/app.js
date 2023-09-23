01129c09b699dccdfd533e08edcb4523b41c1f94
ElenBhattarai <elenbhattarai2002@ku.edu>
2023-09-23 13:33:40 -0500

    init commit backend

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+app.use(cors());
+app.use(bodyParser.json());
+const e = require('express');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(8000, ()=> {
+    console.log("Backend is running")
+})
